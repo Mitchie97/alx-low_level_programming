fcf30f198ffaecaeb961f691190ab2db0feeadc8
Mitchie97 <mitchelllebogang@gmail.com>
2023-06-30 06:42:30 -0700

    Task 8:Edit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * rot13 - encoder rot13
  * @s: pointer to spring params
@@ -12,9 +11,9 @@ char *rot13(char *s)
 	int j;
 	char data1[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] =		
+	char datarot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	for (i = 0; s[1] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
